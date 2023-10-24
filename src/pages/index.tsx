325cfdd59f5dbfaea81bea4f5f7c2326ab8b8da0
Eduardo Hessel <edu.hessel@hotmail.com>
2023-10-24 15:29:59 -0300

    v1.02
    Alterações no Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
+import Image from 'next/image'
 import styles from 'styles/home.module.scss'
 
 const HomePage = () => (
   <div className={styles.container}>
     <header>
-      <div>header</div>
+      <div className={styles.container}>
+        <Image
+          src="/riot-games.svg"
+          width={52}
+          height={26}
+          alt="Logo da riot games"
+        />
+        <span />
+
+        <Image
+          src="/valorant.svg"
+          width={32}
+          height={26}
+          alt="Logo do valorant"
+        />
+
+        <nav>nav</nav>
+
+        <div>user</div>
+      </div>
     </header>
     <main>main</main>
   </div>
