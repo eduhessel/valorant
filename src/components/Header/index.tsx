3796e3753b9ae39c30901bf19d805d054a9c9c24
Eduardo Hessel <edu.hessel@hotmail.com>
2023-10-24 16:06:15 -0300

    v1.02
    Creating Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import styles from './styles.module.scss'
+import { Triangle } from '@phosphor-icons/react'
+
+export const Header = () => (
+  <header className={styles.container}>
+    <div className={styles.header__content}>
+      <Image
+        src="/riot-games.svg"
+        width={52}
+        height={26}
+        alt="Logo da riot games"
+      />
+      <span className={styles.divider} />
+
+      <Image
+        src="/valorant.svg"
+        width={32}
+        height={26}
+        alt="Logo do valorant"
+      />
+
+      <nav className={styles.nav}>
+        <a href="#">GAME</a>
+        <a href="#">CHAMPIONS</a>
+        <a href="#">NEWS</a>
+        <a href="#">PATCH NOTES</a>
+        <a href="#">SHOP</a>
+        <a href="#">SUPORT</a>
+      </nav>
+
+      <div className={styles.user}>
+        <picture>
+          <Image
+            src="/avatar.jpeg"
+            alt="Imagem de avatar do usuário"
+            layout="fill"
+          />
+        </picture>
+
+        <strong>Leo Vargas</strong>
+        <Triangle size={8} weight="fill" />
+      </div>
+    </div>
+  </header>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,9 @@
-import Image from 'next/image'
+import { Header } from 'components/Header'
 import styles from 'styles/home.module.scss'
 
 const HomePage = () => (
   <div className={styles.container}>
-    <header>
-      <div className={styles.container}>
-        <Image
-          src="/riot-games.svg"
-          width={52}
-          height={26}
-          alt="Logo da riot games"
-        />
-        <span />
-
-        <Image
-          src="/valorant.svg"
-          width={32}
-          height={26}
-          alt="Logo do valorant"
-        />
-
-        <nav>nav</nav>
-
-        <div>user</div>
-      </div>
-    </header>
+    <Header />
     <main>main</main>
   </div>
 )
